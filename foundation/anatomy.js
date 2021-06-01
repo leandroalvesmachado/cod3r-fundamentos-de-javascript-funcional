bac684fbffaa25ed1e72a1e0d281940bf5b6763c
leandroalvesmachado <leandroalvesmachado@gmail.com>
2021-06-01 15:35:32 -0300

    Iniciando

---
+++ foundation/anatomy.js
@@ -0,0 +1,13 @@
+// Function declaration
+
+function sayHello() {
+    console.log('Hello!')
+}
+
+sayHello()
+
+function sayHelloTo(name) {
+    console.log(`Hello ${name}`);
+}
+
+sayHelloTo('Leandro')
