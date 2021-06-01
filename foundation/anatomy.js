3dc24d01f98f53c4d3dd085a62f503b43aa784eb
leandroalvesmachado <leandroalvesmachado@gmail.com>
2021-06-01 15:39:43 -0300

    Salvando aula

--- foundation/anatomy.js
+++ foundation/anatomy.js
@@ -6,8 +6,27 @@ function sayHello() {
 
 sayHello()
 
+
 function sayHelloTo(name) {
     console.log(`Hello ${name}`);
 }
 
-sayHelloTo('Leandro')
+sayHelloTo('Leandro sayHelloTo')
+
+
+function returnHi() {
+    return 'Hi!'
+}
+
+let greeting1 = returnHi()
+const greeting2 = returnHi()
+console.log(greeting1)
+console.log(greeting2)
+console.log(returnHi())
+
+
+function returnHiTo(name) {
+    return `Hi ${name}`
+}
+
+console.log(returnHiTo('Leandro returnHiTo'))
