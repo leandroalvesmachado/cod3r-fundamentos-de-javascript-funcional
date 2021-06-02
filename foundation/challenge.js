fc15feaad749eeee68c829057afa683193c79ffe
leandroalvesmachado <leandroalvesmachado@gmail.com>
2021-06-02 15:35:08 -0300

    Fim do desafio

--- foundation/challenge.js
+++ foundation/challenge.js
@@ -1,13 +1,11 @@
 // Create a range function
-
-// range(10, 19, 2) -> [10, 12, 14, 16, 18]
-// range(6, 2) -> [6, 5, 4, 3, 2]
-// range(8, -3, 4) -> [8, 4, 0]
-
-function range(a, b, s) {
+function range(a, b, s = 1) {
     const nums = []
+    const n1 = b === undefined ? 1 : a
+    const n2 = b === undefined ? a : b
+    const step = n1 <= n2 ? Math.abs(s) : -Math.abs(s)
 
-    for (let i = 1; i <= a; i++) {
+    for (let i = n1; n1 <= n2 ? i <= n2 : i >= n2; i += step) {
         nums.push(i);
     }
 
@@ -19,3 +17,12 @@ console.log(range(5))
 
 // range(6, 11) -> [6, 7, 8, 9, 10, 11]
 console.log(range(6, 11))
+
+// range(10, 19, 2) -> [10, 12, 14, 16, 18]
+console.log(range(10, 19, 2))
+
+// range(6, 2) -> [6, 5, 4, 3, 2]
+console.log(range(6, 2))
+
+// range(8, -3, 4) -> [8, 4, 0]
+console.log(range(8, -3, 4))
