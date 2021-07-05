0884d197f21dcbbfc747d503214c823dccce00ac
leandroalvesmachado <leandroalvesmachado@gmail.com>
2021-07-05 11:05:24 -0300

    Iniciando Promise 2

--- funcoes/callback_5.js
+++ funcoes/callback_5.js
@@ -30,3 +30,26 @@ const totalGeralComReduce = carrinho
 // 224.96
 console.log(totalGeralComReduce)
 
+
+// implementacao do meuReduce
+Array.prototype.meuReduce = function(fn, inicial) {
+    let acc = inicial
+
+    for (let i = 0; i < this.length; i ++) {
+        if (!acc && i === 0) {
+            acc = this[i]
+        } else {
+            acc = fn(acc, this[i], i, this)
+        }
+    }
+
+    return acc
+}
+
+// 224.96
+const totalGeral3 = carrinho
+    .map(getTotal)
+    .reduce(somar)
+
+console.log(totalGeral3)
+
