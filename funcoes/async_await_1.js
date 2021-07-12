4186784abb9852bf3b080a9d9b0b65fe2ccc0350
leandroalvesmachado <leandroalvesmachado@gmail.com>
2021-07-12 16:08:30 -0300

    iniciando async await 2

--- funcoes/async_await_1.js
+++ funcoes/async_await_1.js
@@ -15,18 +15,34 @@ function esperarPor(tempo = 2000) {
 //     .then(esperarPor)
 //     .then(() => console.log('Executando promise 3...'))
 
+function retornarValor(tempo = 2000) {
+    return new Promise(function(resolve) {
+        setTimeout(() => resolve(10), 5000)
+    })
+} 
+
 // fazendo mesmo codigo com async/await ()
 // executa na ordem, sem async/await executa tudo de uma vez
-// executa de forma sincrona
+// await (esperar)
+// async await precisa de uma promise
 async function executar() {
-    await esperarPor(2000)
-    console.log('Async/Await 1...')
+    // espera 5 segundos e executa e depois prossegue
+    let valor = await retornarValor()
 
     await esperarPor(2000)
-    console.log('Async/Await 2...')
+    console.log(`Async/Await 1...${valor}`)
 
     await esperarPor(2000)
-    console.log('Async/Await 3...')
+    console.log(`Async/Await 2...${valor + 1}`)
+
+    await esperarPor(2000)
+    console.log(`Async/Await 3...${valor + 2}`)
 }
 
 executar()
+
+// nao funciona, pois é uma promise
+// a variavel vai ser criada antes da funcao terminar de executar
+// promise ainda nao resolvida
+// const v = executar()
+// executar().then(console.log)
