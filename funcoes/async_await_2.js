d42473574ddfb2dedad8715f42bf7bd8e6e185e2
leandroalvesmachado <leandroalvesmachado@gmail.com>
2021-07-13 14:58:48 -0300

    Fim da aula #00 1

--- funcoes/async_await_2.js
+++ funcoes/async_await_2.js
@@ -1,9 +1,45 @@
-function gerarNumerosEntre(min, max) {
+function gerarNumerosEntre(min, max, numerosProibidos) {
     if (min > max) [max, min] = [min, max]
 
-    return new Promise(resolve => {
+    return new Promise((resolve, reject) => {
         const fator = max - min + 1
         const aleatorio = parseInt(Math.random() * fator) + min
-        resolve(aleatorio)
+
+        if (numerosProibidos.includes(aleatorio)) {
+            reject('Número repetido!')
+        } else {
+            resolve(aleatorio)
+        }
     })
 }
+
+// gera um array vazio
+// of Array(qtdeNumeros).fill()
+// await fica na função que retorna uma promise
+
+async function gerarMegaSena(qtdeNumeros, tentativas = 1) {
+    try {
+        const numeros = []
+    
+        for (let _ of Array(qtdeNumeros).fill()) {
+            numeros.push(await gerarNumerosEntre(1, 60, numeros))
+        }
+
+        return numeros   
+    } catch (e) {
+        // throw "Que chato!!!"
+        if (tentativas > 10) {
+            throw "Não deu certo!!!"
+        } else {
+            return gerarMegaSena(qtdeNumeros, tentativas + 1)
+        }
+    }
+}
+
+// gerarNumerosEntre(1, 5, [1, 2, 4])
+//     .then(console.log)
+//     .catch(console.log)
+
+gerarMegaSena(8)
+    .then(console.log)
+    .catch(console.log)
---
+++ funcoes/oo_1.js
@@ -0,0 +1,30 @@
+function Produto(nome, preco, desc = 0.15) {
+    // atributos publicos
+    this.nome = nome
+    this.preco = preco
+    this.desc = desc
+
+    this.precoFinal = function() {
+        return this.preco * (1 - this.desc)
+    }
+}
+
+const p1 = new Produto('Caneta', 8.59)
+// Produto {} objeto do type produto
+// Produto { nome: 'Caneta', preco: 8.59 }
+console.log(p1)
+
+// object
+console.log(typeof p1)
+
+const p2 = new Produto('Geladeira', 2345.98)
+
+// Produto { nome: 'Geladeira', preco: 2345.98 }
+console.log(p2)
+
+// Geladeira
+console.log(p2.nome)
+
+// 1994.0829999999999
+console.log(p2.precoFinal())
+
